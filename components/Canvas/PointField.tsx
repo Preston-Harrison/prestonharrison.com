9c637235ba4a06af6513af645693111da86cd16c
Preston Harrison <preston@prestonharrison.com>
2022-04-11 09:07:22 +1000

    fix opacity on background

--- components/Canvas/PointField.tsx
+++ components/Canvas/PointField.tsx
@@ -6,29 +6,28 @@ const PointField = () => {
   const pointsRef = React.useRef<THREE.Points>();
   const [geometry, setGeometry] = React.useState<THREE.BufferGeometry>();
   const [material, setMaterial] = React.useState<THREE.PointsMaterial>();
-  
+
   React.useEffect(() => {
     const loader = new PLYLoader();
     loader.load('/assets/point_cloud_model.ply', (geometry) => {
-        const sprite = new THREE.TextureLoader().load( '/assets/disc.png' );
-        const material = new THREE.PointsMaterial({
-            size: 0.01,
-            map: sprite,
-            alphaTest: 0.5,
-            transparent: true,
-            opacity: 0.7
-        })
-        material.vertexColors = true;
-        setMaterial(material);
-        setGeometry(geometry);
+      const sprite = new THREE.TextureLoader().load('/assets/disc.png');
+      const material = new THREE.PointsMaterial({
+        size: 0.01,
+        map: sprite,
+        transparent: true,
+        opacity: 0.4,
+      })
+      material.vertexColors = true;
+      setMaterial(material);
+      setGeometry(geometry);
     });
   }, []);
 
   return (
-    <points 
-        ref={pointsRef}
-        geometry={geometry}
-        material={material}
+    <points
+      ref={pointsRef}
+      geometry={geometry}
+      material={material}
     />
   )
 }
